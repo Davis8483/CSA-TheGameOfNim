2d45a3c5ab5f58c45d6900a70e676d1712873af6
Noah Davis <davisprints3d@gmail.com>
2024-03-01 19:22:11 -0600

    Project Completion

--- Board.java
+++ Board.java
@@ -7,7 +7,7 @@ public class Board {
     public static int getNumPieces(){return numPieces;}
     public static Boolean removePieces(int value){
         // checks if value is positive, and if value is between 0 and (numPieces/2)
-        if((Integer.signum(value) == 1) && (numPieces-value > 0) && (value < (int)numPieces/2)){
+        if((Integer.signum(value) == 1) && (numPieces-value > 0) && (value <= (int)numPieces/2)){
             numPieces -= value;
             // pieces were subtracted correctly
             return true;
--- Game.java
+++ Game.java
@@ -1,21 +1,39 @@
 import java.util.Scanner;  // Import the Scanner class
 
 public class Game {
+    Scanner sc = new Scanner(System.in);  // Create a Scanner object
+
     public void play(){
-        Scanner sc = new Scanner(System.in);  // Create a Scanner object
         int currentPlayer = 0;
         Player[] players = {new Player(), new Player()}; // initialize players
 
         // main game loop
         while(true){
             System.out.println(String.format("%s, its your turn!", players[currentPlayer].name));
+            System.out.println(String.format("%s pieces remain...", Board.getNumPieces()));
             System.out.print(String.format("How many pieces would you like to take? (1-%s): ", (int)Board.getNumPieces()/2));
 
-            Board.removePieces(sc.nextInt());
+            while(!Board.removePieces(getIntegerInput())){
+                System.out.print(String.format("Invalid Input, your input must be within range (1-%s): ", (int)Board.getNumPieces()/2));
+            }
 
             // toggle between player 0 and 1
             if(currentPlayer == 0){currentPlayer = 1;}
             else{currentPlayer = 0;}
+
+            if(Board.getNumPieces() == 1){
+                System.out.println(String.format("%s won the game!", players[currentPlayer].name));
+                break;
+            }
+        }
+    }
+    public int getIntegerInput(){
+        try{
+            return sc.nextInt();
+        }
+        catch(Exception e){
+            sc.nextLine();
+            return 0;
         }
     }
 }
